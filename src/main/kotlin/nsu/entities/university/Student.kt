b4d5f2e314556bba3e2aea55df1e0a7f6386b054
L3XxXa <alexkkk@inbox.ru>
2022-11-06 16:33:02 +0700

    Student entity

---
+++ src/main/kotlin/nsu/entities/university/Student.kt
@@ -0,0 +1,12 @@
+package nsu.entities.university
+
+class Student(val name: String) {
+    private val name: String
+        get() {
+            return field
+        }
+
+    init {
+        this.name = name
+    }
+}
