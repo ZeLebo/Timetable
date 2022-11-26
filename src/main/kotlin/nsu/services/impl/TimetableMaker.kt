4d857d0762b874ed7579abe09fc05a19fff3fe56
L3XxXa <alexkkk@inbox.ru>
2022-11-26 23:37:52 +0700

    Get some entities

--- src/main/kotlin/nsu/services/impl/TimetableMaker.kt
+++ src/main/kotlin/nsu/services/impl/TimetableMaker.kt
@@ -1,11 +1,35 @@
 package nsu.services.impl
 
-import nsu.entities.university.Lesson
-import nsu.entities.university.Room
-import nsu.entities.university.UniversityPlan
+import nsu.entities.university.*
 
 // this is service for creating timetable
 
-class TimetableMaker(private val rooms: ArrayList<Room>, private val lessons: ArrayList<Lesson>, private val universityPlan: UniversityPlan, ) {
+class TimetableMaker(universityPlan: UniversityPlan, private val rooms: ArrayList<Room>) {
+    private val faculties: ArrayList<Faculty> = universityPlan.faculties
+    private val specializationsByFaculties: LinkedHashMap<Faculty, ArrayList<Specialization>> = LinkedHashMap()
+    private val studyYearByFaculty: LinkedHashMap<Faculty, ArrayList<StudyYear>> = LinkedHashMap()
 
+    init {
+        getSpecializationByFaculty()
+        getStudyYearByFaculty()
+    }
+
+    private fun getSpecializationByFaculty() {
+        faculties.forEach {
+            specializationsByFaculties[it] = it.specializations
+        }
+    }
+
+    private fun getStudyYearByFaculty(){
+        faculties.forEach {
+            val faculty: Faculty = it
+            it.specializations.forEach { y ->
+                studyYearByFaculty[faculty] = y.studyYears
+            }
+        }
+    }
+
+}
+
+fun main() {
 }
