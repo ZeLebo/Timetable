b6361e6ec6259a6acdc48ad48f5094f1d542df73
L3XxXa <alexkkk@inbox.ru>
2022-11-27 17:38:41 +0700

    Making useful data

--- src/main/kotlin/nsu/services/impl/TimetableMaker.kt
+++ src/main/kotlin/nsu/services/impl/TimetableMaker.kt
@@ -1,35 +1,90 @@
 package nsu.services.impl
 
+import nsu.entities.people.Teacher
 import nsu.entities.university.*
 
 // this is service for creating timetable
 
-class TimetableMaker(universityPlan: UniversityPlan, private val rooms: ArrayList<Room>) {
+class TimetableMaker(universityPlan: UniversityPlan, private val rooms: ArrayList<Room>, private val teachers: ArrayList<Teacher>) {
     private val faculties: ArrayList<Faculty> = universityPlan.faculties
-    private val specializationsByFaculties: LinkedHashMap<Faculty, ArrayList<Specialization>> = LinkedHashMap()
-    private val studyYearByFaculty: LinkedHashMap<Faculty, ArrayList<StudyYear>> = LinkedHashMap()
+    private val specializationsByFaculties: LinkedHashMap<Faculty, ArrayList<Specialization>>
+    private val studyYearByFaculty: LinkedHashMap<Faculty, ArrayList<StudyYear>>
+    private val subjectsBySpecialization: LinkedHashMap<Specialization, ArrayList<Subject>>
+
 
     init {
-        getSpecializationByFaculty()
-        getStudyYearByFaculty()
+        specializationsByFaculties = getSpecializationByFaculty(faculties)
+        studyYearByFaculty = getStudyYearByFaculty(faculties)
+        subjectsBySpecialization = getSubjectBySpecialization(faculties)
     }
 
-    private fun getSpecializationByFaculty() {
+    private fun getSubjectBySpecialization(faculties: ArrayList<Faculty>): LinkedHashMap<Specialization, ArrayList<Subject>> {
+        val subjectsBySpecialization: LinkedHashMap<Specialization, ArrayList<Subject>> = LinkedHashMap()
+        faculties.forEach {
+            it.specializations.forEach{y ->
+                y.studyYears.forEach {x->
+                    subjectsBySpecialization[y] = x.subjects
+                }
+            }
+        }
+        return subjectsBySpecialization
+    }
+
+    private fun getSpecializationByFaculty(faculties: ArrayList<Faculty>): LinkedHashMap<Faculty, ArrayList<Specialization>> {
+        val specializationsByFaculties: LinkedHashMap<Faculty, ArrayList<Specialization>> = LinkedHashMap()
         faculties.forEach {
             specializationsByFaculties[it] = it.specializations
         }
+        return specializationsByFaculties
     }
 
-    private fun getStudyYearByFaculty(){
+    private fun getStudyYearByFaculty(faculties: ArrayList<Faculty>): LinkedHashMap<Faculty, ArrayList<StudyYear>> {
+        val studyYearByFaculty: LinkedHashMap<Faculty, ArrayList<StudyYear>> = LinkedHashMap()
         faculties.forEach {
             val faculty: Faculty = it
             it.specializations.forEach { y ->
                 studyYearByFaculty[faculty] = y.studyYears
             }
         }
+        return studyYearByFaculty
+    }
+
+    fun getEverything(){
+        println(specializationsByFaculties[faculties[0]])
     }
 
 }
 
 fun main() {
+    val lessonsForMatanForNewThread: ArrayList<Lesson> = ArrayList()
+    lessonsForMatanForNewThread.add(Lesson("Матан", "Лекционная", "Лекция"))
+    lessonsForMatanForNewThread.add(Lesson("Матан","Семинарская", "Семинар"))
+
+    val lessonsForImperativka: ArrayList<Lesson> = ArrayList()
+    lessonsForImperativka.add(Lesson("Imperativka", "Lekcionnaya", "Lekciya"))
+    lessonsForImperativka.add(Lesson("Imperativka", "Seminarskaya", "Seminar"))
+
+    val subjectsForNewThread: ArrayList<Subject> = ArrayList()
+    subjectsForNewThread.add(Subject(lessonsForMatanForNewThread, "Матан"))
+    subjectsForNewThread.add(Subject(lessonsForImperativka, "Imperativka"))
+
+    val studyYearsForFit: ArrayList<StudyYear> = ArrayList()
+    studyYearsForFit.add(StudyYear(subjectsForNewThread, 1))
+    val fitSpecializations: ArrayList<Specialization> = ArrayList()
+    fitSpecializations.add(Specialization(studyYearsForFit, "New Thread()"))
+    val faculties: ArrayList<Faculty> = ArrayList()
+    faculties.add(Faculty(fitSpecializations, "FIT"))
+    val universityPlan = UniversityPlan(faculties)
+
+    val rooms: ArrayList<Room> = ArrayList()
+    rooms.add(Room(30, "Лекционная"))
+    rooms.add(Room(10, "Семинарская"))
+
+    val teachers: ArrayList<Teacher> = ArrayList()
+    val linkedHashMap: LinkedHashMap<Subject, String> = LinkedHashMap()
+    linkedHashMap[Subject(lessonsForMatanForNewThread, "Матан")] = "Семинары"
+    teachers.add(Teacher("Abobus Abobusovich",linkedHashMap))
+
+    val timetableMaker = TimetableMaker(universityPlan, rooms, teachers)
+    timetableMaker.getEverything()
 }
