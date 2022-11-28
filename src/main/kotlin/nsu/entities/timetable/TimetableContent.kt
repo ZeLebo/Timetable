f4c066b45c9abca9da557b62e598b5dbd40c1b13
L3XxXa <alexkkk@inbox.ru>
2022-11-28 18:45:18 +0700

    Timetable entity

--- src/main/kotlin/nsu/entities/timetable/TimetableContent.kt
+++ src/main/kotlin/nsu/entities/timetable/TimetableContent.kt
@@ -1,3 +1,17 @@
 package nsu.entities.timetable
 
-data class TimetableContent (val timetable: String)
+import nsu.entities.people.Group
+import nsu.entities.people.Teacher
+import nsu.entities.university.Room
+
+/**
+ * Lesson in timetable entity.
+ *
+ * @property discipline - name of discipline
+ * @property day - weekday of the lesson
+ * @property hour - time of the lesson
+ * @property teacher - name of the teacher
+ * @property group - group which will attend the lesson
+ * @property room - room where the lesson will be
+ * */
+data class TimetableContent (val discipline: String, val day: String, val hour: Int, val teacher: Teacher, val group: ArrayList<Group>, val room: Room)
