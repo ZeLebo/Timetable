361a4fc34443ccad9144309f256c8a7ccb0229c8
L3XxXa <alexkkk@inbox.ru>
2022-11-28 18:28:27 +0700

    Added property groups for study year

--- src/main/kotlin/nsu/entities/university/StudyYear.kt
+++ src/main/kotlin/nsu/entities/university/StudyYear.kt
@@ -1,7 +1,11 @@
 package nsu.entities.university
+
+import nsu.entities.people.Group
+
 /**
  * Study Year (course) entity
  * @property subjects - list of the subjects during this year
- * @property year - year of incoming???
+ * @property year - course of studying
+ * @property groups - list of groups on this course in faculty
  * */
-data class StudyYear (val subjects: ArrayList<Subject>, val year: Int)
+data class StudyYear (val subjects: ArrayList<Subject>, val year: Int, val groups: ArrayList<Group>)
