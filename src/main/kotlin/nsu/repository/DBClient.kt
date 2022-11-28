cf44cf06bd2d39d2cae4ae20e3088d7ea3abb33a
vovademarnsu <v.medvedev@g.nsu.ru>
2022-11-28 18:02:18 +0700

    aasd

--- src/main/kotlin/nsu/repository/DBClient.kt
+++ src/main/kotlin/nsu/repository/DBClient.kt
@@ -1,14 +1,43 @@
 package nsu.repository
 
 import java.sql.DriverManager
+import nsu.entities.people.Student
+
+//create class
+class DBClient {
+
+    //create function
+    fun connect() {
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/timetable_db", "admin", "admin")
+
+    }
+
+    //create table
+    fun createTable() {
+        //create connection
+        val connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/timetable_db", "admin", "admin")
+        //create statement
+        val statement = connection.createStatement()
+        //execute query
+        statement.execute("CREATE TABLE students (id SERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), group_id INT)")
+    }
 
 
+}
 fun main() {
 
-    val jdbcUrl = "jdbc:postgresql://localhost:5432/timetable_db"
+//    //create object
+//    val dbClient = DBClient()
+//    //call function
+//    val connection = dbClient.connect()
+//    println(connection.isValid(0))
+
+    val dbName = "timetable_db"
+    val jdbcUrl = "jdbc:postgresql://localhost:5432/${dbName}"
     val connection = DriverManager.getConnection(jdbcUrl, "admin", "admin")
 
     data class Students(val id: Int, val first_name: String, val last_name: String, val group_id: Int)
+
     val query = connection.prepareStatement("SELECT * from students")
     val result = query.executeQuery()
     val students = mutableListOf<Students>()
@@ -23,6 +52,7 @@ fun main() {
 
 
     data class Groups(val id: Int, val name: String)
+
     val groups = mutableListOf<Groups>()
     val queryForGroups = connection.prepareStatement("SELECT * from groups")
     val resultForGroups = queryForGroups.executeQuery()
