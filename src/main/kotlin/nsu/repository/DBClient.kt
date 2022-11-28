7df52162bbdd1768f8f0f52766be150945f56152
vovademarnsu <v.medvedev@g.nsu.ru>
2022-11-28 18:48:15 +0700

    client

--- src/main/kotlin/nsu/repository/DBClient.kt
+++ src/main/kotlin/nsu/repository/DBClient.kt
@@ -2,68 +2,83 @@ package nsu.repository
 
 import java.sql.DriverManager
 import nsu.entities.people.Student
+import java.sql.Connection
 
 //create class
 class DBClient {
 
     //create function
-    fun connect() {
-        DriverManager.getConnection("jdbc:postgresql://localhost:5432/timetable_db", "admin", "admin")
-
+    private fun connect(): Connection {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/timetable_db", "admin", "admin")
     }
 
-    //create table
-    fun createTable() {
+    //create table students
+    fun createTableStudents() {
         //create connection
-        val connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/timetable_db", "admin", "admin")
+        val connection = connect()
         //create statement
         val statement = connection.createStatement()
         //execute query
-        statement.execute("CREATE TABLE students (id SERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), group_id INT)")
+        try {
+            statement.execute("CREATE TABLE students (id SERIAL PRIMARY KEY, first_name VARCHAR(255), second_name VARCHAR(255), group_id INT)")
+        } catch (e: Exception) {
+            println("Table students already exists")
+        }
     }
 
+    //create table groups
+    fun createTableGroups() {
+        //create connection
+        val connection = connect()
+        //create statement
+        val statement = connection.createStatement()
+        //execute query
+        try {
+            statement.execute("CREATE TABLE groups (id SERIAL PRIMARY KEY, name VARCHAR(255))")
+        } catch (e: Exception) {
+            println("Table group already exists")
+        }
+    }
 
+    //add record to table groups
+    fun addGroup(name: String) {
+        //create connection
+        val connection = connect()
+        //create statement
+        val statement = connection.createStatement()
+        //execute query
+        statement.execute("INSERT INTO groups (name) VALUES ('$name')")
+    }
+
+    //add record to table students
+    fun addStudent(student: Student) {
+        //create connection
+        val connection = connect()
+        //create statement
+        val statement = connection.createStatement()
+        //execute query
+        statement.execute("INSERT INTO students (first_name, last_name, group_id) VALUES ('${student.first_name}', '${student.second_name}', ${student.group_id})")
+    }
+
+    //get all records from table
+    fun getAllStudents(): List<Student> {
+        //create connection
+        val connection = connect()
+        //create statement
+        val statement = connection.createStatement()
+        //execute query
+        val resultSet = statement.executeQuery("SELECT * FROM students")
+        //create list of students
+        val students = mutableListOf<Student>()
+        //iterate over result set
+        while (resultSet.next()) {
+            //create student
+            val student = Student(resultSet.getString("first_name"), resultSet.getString("last_name"), resultSet.getInt("id"), resultSet.getInt("group_id"))
+            //add student to list
+            students.add(student)
+        }
+        //return list of students
+        return students
+    }
 }
-fun main() {
 
-//    //create object
-//    val dbClient = DBClient()
-//    //call function
-//    val connection = dbClient.connect()
-//    println(connection.isValid(0))
-
-    val dbName = "timetable_db"
-    val jdbcUrl = "jdbc:postgresql://localhost:5432/${dbName}"
-    val connection = DriverManager.getConnection(jdbcUrl, "admin", "admin")
-
-    data class Students(val id: Int, val first_name: String, val last_name: String, val group_id: Int)
-
-    val query = connection.prepareStatement("SELECT * from students")
-    val result = query.executeQuery()
-    val students = mutableListOf<Students>()
-
-    while (result.next()) {
-        val id = result.getInt("id")
-        val firstName = result.getString("first_name")
-        val lastName = result.getString("last_name")
-        val groupId = result.getInt("group_id")
-        students.add(Students(id, firstName, lastName, groupId))
-    }
-
-
-    data class Groups(val id: Int, val name: String)
-
-    val groups = mutableListOf<Groups>()
-    val queryForGroups = connection.prepareStatement("SELECT * from groups")
-    val resultForGroups = queryForGroups.executeQuery()
-
-    while (resultForGroups.next()) {
-        val id = resultForGroups.getInt("id")
-        val name = resultForGroups.getString("name")
-        groups.add(Groups(id, name))
-    }
-
-    println(students)
-    println(groups)
-
-}
