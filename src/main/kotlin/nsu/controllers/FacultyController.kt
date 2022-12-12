c5b2e0fda02aa4892740ad4f38c21f1c8aa11833
ZhoRa <sartakov.sasha@icloud.com>
2022-12-12 22:52:20 +0700

    Little f* up

--- src/main/kotlin/nsu/controllers/FacultyController.kt
+++ src/main/kotlin/nsu/controllers/FacultyController.kt
@@ -1,9 +1,7 @@
 package nsu.controllers
 
 import nsu.entities.university.Faculty
-import nsu.entities.university.Specialization
 import nsu.service.FacultyService
-import nsu.service.SpecializationService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1")
 class FacultyController(
     private val facultyService: FacultyService,
-    private val specializationService: SpecializationService,
 ) {
     //get the list of all faculties
     @GetMapping("faculty")
@@ -37,33 +34,6 @@ class FacultyController(
             ?: return ResponseEntity.badRequest().body("No such faculty")
 
         return ResponseEntity.ok(faculty)
-    }
-
-    //add specialization to the faculty
-    @PostMapping("faculty/{facultyId}/specialization")
-    fun addSpecialization(@PathVariable facultyId: Int, @RequestBody specialization: Specialization): ResponseEntity<*> {
-        val faculty = facultyService.findByID(facultyId.toLong())
-            ?: return ResponseEntity.badRequest().body("No such faculty")
-
-        val spec = specializationService.addSpecialization(specialization)
-        faculty.specializations.add(spec)
-        return ResponseEntity.ok(facultyService.updateFaculty(faculty))
-    }
-
-    // delete specialization from the faculty
-    @DeleteMapping("faculty/{facultyId}/specialization/{specializationId}")
-    fun deleteSpecialization(@PathVariable facultyId: Int, @PathVariable specializationId: Int): ResponseEntity<*> {
-        val faculty = facultyService.findByID(facultyId.toLong())
-            ?: return ResponseEntity.badRequest().body("No such faculty")
-
-        val specialization = specializationService.findByID(specializationId.toLong())
-            ?: return ResponseEntity.badRequest().body("No such specialization")
-
-        faculty.specializations.remove(specialization)
-        // the below line is needed in case we want to delete the specialization from the database
-//        specializationService.delete(specializationId.toLong())
-
-        return ResponseEntity.ok(facultyService.updateFaculty(faculty))
     }
 
     // delete faculty
