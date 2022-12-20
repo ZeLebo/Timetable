9a12e2f5418c8a1c8c95d35dc37b0a886d27b9c1
L3XxXa <alexkkk@inbox.ru>
2022-12-20 21:09:51 +0700

    Can get data from db

--- src/main/kotlin/nsu/service/impl/TimetableMaker.kt
+++ src/main/kotlin/nsu/service/impl/TimetableMaker.kt
@@ -1,5 +1,7 @@
 package nsu.service.impl
 
+import nsu.entities.people.Group
+import nsu.entities.people.Teacher
 import nsu.entities.university.*
 import nsu.service.*
 
@@ -22,18 +24,41 @@ class TimetableMaker(
     private val specializations: List<Specialization>
     private val studyYears: List<StudyYear>
     private val subjects: List<Subject>
+    private val teachers: List<Teacher>
+    private val rooms: List<Room>
+    private val groups: List<Group>
+    private val lessons: List<Lesson>
 
     init {
         faculties = findFaculties()
         specializations = findSpecializations()
         studyYears = findStudyYears()
         subjects = findSubjects()
+        teachers = findTeachers()
+        rooms = findRooms()
+        groups = findGroups()
+        lessons = findLessons()
+    }
+
+    private fun findLessons(): List<Lesson> {
+        return lessonService.findAll()
+    }
+
+    private fun findGroups(): List<Group> {
+        return groupService.findAll()
+    }
+
+    private fun findRooms(): List<Room> {
+        return roomService.findAll()
+    }
+
+    private fun findTeachers(): List<Teacher> {
+        return teacherService.findAll()
     }
 
     private fun findSubjects(): List<Subject> {
         return subjectService.findAll()
     }
-
 
     private fun findStudyYears(): List<StudyYear> {
         return studyYearService.findAll()
