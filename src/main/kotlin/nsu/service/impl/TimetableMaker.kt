d976637dc24dcd74601350d540a0c815869bcb6d
L3XxXa <alexkkk@inbox.ru>
2022-12-20 21:02:24 +0700

    Getting data via services

--- src/main/kotlin/nsu/service/impl/TimetableMaker.kt
+++ src/main/kotlin/nsu/service/impl/TimetableMaker.kt
@@ -1,25 +1,49 @@
 package nsu.service.impl
 
-import nsu.entities.people.Group
-import nsu.entities.people.Teacher
 import nsu.entities.university.*
-import nsu.repository.FacultyRepository
+import nsu.service.*
 
 // this is service for creating timetable
 /**
  *
  * */
-class TimetableMaker() {
-    private val faculties: ArrayList<Faculty>
-
-    private val csandsdSpecialization = Specialization("")
+class TimetableMaker(
+    private val facultyService: FacultyService,
+    private val groupService: GroupService,
+    private val lessonService: LessonService,
+    private val roomService: RoomService,
+    private val specializationService: SpecializationService,
+    private val studyYearService: StudyYearService,
+    private val subjectService: SubjectService,
+    private val teacherService: TeacherService,
+    private val timetableContentService: TimetableContentService
+) {
+    private val faculties: List<Faculty>
+    private val specializations: List<Specialization>
+    private val studyYears: List<StudyYear>
+    private val subjects: List<Subject>
 
     init {
-        faculties = getFaculties()
+        faculties = findFaculties()
+        specializations = findSpecializations()
+        studyYears = findStudyYears()
+        subjects = findSubjects()
     }
 
-    private fun getFaculties(): ArrayList<Faculty>{
-        val newFaculties = ArrayList<Faculty>()
-        newFaculties.add(Faculty("ФИТ", , ))
+    private fun findSubjects(): List<Subject> {
+        return subjectService.findAll()
     }
-}B
+
+
+    private fun findStudyYears(): List<StudyYear> {
+        return studyYearService.findAll()
+    }
+
+    private fun findSpecializations(): List<Specialization> {
+        return specializationService.findAll()
+    }
+
+    private fun findFaculties(): List<Faculty> {
+        return facultyService.findAll()
+    }
+}
