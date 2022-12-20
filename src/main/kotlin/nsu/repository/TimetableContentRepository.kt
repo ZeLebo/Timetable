fbb8e9c972cba941cf63a0f00111d62c20e76c02
Вальдемар Медведев <valdemar@MacBook-Pro-Valdemar.local>
2022-12-20 12:28:35 +0700

    Ya gey

--- src/main/kotlin/nsu/repository/TimetableContentRepository.kt
+++ src/main/kotlin/nsu/repository/TimetableContentRepository.kt
@@ -4,5 +4,13 @@ import nsu.entities.timetable.TimetableContent
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TimetableContentRepository: JpaRepository<TimetableContent, Long> {
-    fun findByDay(day: String): TimetableContent
+    fun findByTimetableContentId(id: Long): TimetableContent?
+    fun existsByTimetableContentId(id: Long): Boolean
+    fun findByDiscipline(discipline: String): MutableList<TimetableContent>
+    fun findByDay(day: String): MutableList<TimetableContent>
+    fun findByTeacherName(teacher: String): MutableList<TimetableContent>
+    fun findByGroupsNumber(group: String): MutableList<TimetableContent>
+    fun findByRoomNumber(room: String): MutableList<TimetableContent>
+    fun findByHour(hour: Int): MutableList<TimetableContent>
+
 }
---
+++ src/main/kotlin/nsu/service/impl/TimetableContentServiceImpl.kt
@@ -0,0 +1,46 @@
+package nsu.service.impl
+
+import nsu.entities.timetable.TimetableContent
+import nsu.repository.TimetableContentRepository
+import nsu.service.TimetableContentService
+
+class TimetableContentServiceImpl(
+    private val timetableContentRepository: TimetableContentRepository
+): TimetableContentService {
+    override fun findSpecialDiscipline(discipline: String): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the discipline is that
+        return all.filter { it.discipline == discipline }.toMutableList()
+    }
+
+    override fun findSpecialDay(day: String): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the day is that
+        return all.filter { it.day == day }.toMutableList()
+    }
+
+    override fun findSpecialTeacher(teacher: String): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the teacher is that
+        return all.filter { it.teacher?.name == teacher }.toMutableList()
+    }
+
+    override fun findSpecialGroup(group: String): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the group is that
+        return all.filter { it.groups.any { it.number == group } }.toMutableList()
+    }
+
+    override fun findSpecialRoom(room: String): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the room is that
+        return all.filter { it.room?.number == room }.toMutableList()
+    }
+
+    override fun findSpecialHour(hour: Int): MutableList<TimetableContent> {
+        val all = timetableContentRepository.findAll()
+        // filter where the hour is that
+        return all.filter { it.hour == hour }.toMutableList()
+    }
+
+}
