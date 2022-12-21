1b3518dcbec98e5c03e0231240723253173ec4e3
L3XxXa <alexkkk@inbox.ru>
2022-12-21 13:05:21 +0700

    Teacher service tab fix

--- src/main/kotlin/nsu/service/TeacherService.kt
+++ src/main/kotlin/nsu/service/TeacherService.kt
@@ -3,15 +3,15 @@ package nsu.service
 import nsu.entities.people.Teacher
 
 interface TeacherService {
-        fun addTeacher(teacher: Teacher): Teacher
-        fun updateTeacher(teacher: Teacher): Teacher
-        fun delete(id: Long)
-        fun findByID(id: Long): Teacher?
-        fun findByName(name: String): Teacher?
-        fun exists(id: Long): Boolean
-        fun exists(name: String): Boolean
-        fun findAll(): List<Teacher>
-        fun updateTeacher(teacherId: Long, teacher: Teacher): Teacher
-        fun addLesson(teacherId: Long, subjectId: Long): Teacher
+    fun addTeacher(teacher: Teacher): Teacher
+    fun updateTeacher(teacher: Teacher): Teacher
+    fun delete(id: Long)
+    fun findByID(id: Long): Teacher?
+    fun findByName(name: String): Teacher?
+    fun exists(id: Long): Boolean
+    fun exists(name: String): Boolean
+    fun findAll(): List<Teacher>
+    fun updateTeacher(teacherId: Long, teacher: Teacher): Teacher
+    fun addLesson(teacherId: Long, subjectId: Long): Teacher
     fun removeLessonFromTeacher(teacherId: Long, subjectId: Long): Teacher
 }
