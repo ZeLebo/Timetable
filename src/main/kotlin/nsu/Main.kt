bd9a5aac92668a10f200a3939b7b51111bd18e94
L3XxXa <alexkkk@inbox.ru>
2023-06-18 14:08:46 +0700

    Cors policy fixed

--- src/main/kotlin/nsu/Main.kt
+++ src/main/kotlin/nsu/Main.kt
@@ -2,9 +2,22 @@ package nsu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
-class TimetableApplication
+class TimetableApplication{
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/v1/auth/login").allowedOrigins("http://localhost:3000")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<TimetableApplication>(*args)
