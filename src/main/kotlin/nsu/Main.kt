753784ec762b9163fcae507390860a04fc0508ac
L3XxXa <alexkkk@inbox.ru>
2023-06-18 14:16:31 +0700

    Added annotation for CORS

--- src/main/kotlin/nsu/Main.kt
+++ src/main/kotlin/nsu/Main.kt
@@ -2,22 +2,9 @@ package nsu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 
 @SpringBootApplication
-class TimetableApplication{
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer? {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/api/v1/auth/login").allowedOrigins("http://localhost:3000")
-            }
-        }
-    }
-}
+class TimetableApplication
 
 fun main(args: Array<String>) {
     runApplication<TimetableApplication>(*args)
